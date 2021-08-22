21c64ce4e265cce89d567d46981e6da6e4650f43
unknown <karthiks061992@gmail.com>
2021-08-22 15:39:54 +0530

    changes

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def login():
 
 #This is main branch for allocation of signup and login API for books using cluster 
 @app.route("/get_details/<book>",methods=['POST','GET'])#Route for getting details of a book
-def get_book(book):
+def get_book_don(book):
     book_retr=list(db.books.find({"name":book},{"_id":0}))
     if(len(book_retr)==0):
         return jsonify("no books were found")
